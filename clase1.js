c9e694ff875a73b95ce66265542ff565ae7758bb
Mariana Isaac <marian.isaac10@gmail.com>
2021-10-30 23:03:10 +0200

    primer desafio

---
+++ clase1.js
@@ -0,0 +1,18 @@
+// Desafio 1 
+
+let nombre = prompt ("Ingrese su nombre");
+let apellido = prompt ("Ingrese su apellido");
+let completo = nombre + apellido;
+let blanco = "";  
+
+console.log("Bienvenido " + blanco + completo);
+
+let dni = prompt ("Ingrese su DNI");  
+
+console.log("Su DNI es: " + blanco + dni)
+
+alert ("Que comience el juego");
+
+
+
+
